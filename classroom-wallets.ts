0be3aa81ffc6a55b702ae197aeaf7a803db692c8
Andrew Lavryshyn <alavry@users.noreply.github.com>
2024-05-28 12:53:10 -0400

    feat: add check-balance and classroom-wallets

---
+++ classroom-wallets.ts
@@ -0,0 +1,7 @@
+const RAM = "6oZN3rKFqDcggXHs8RnQVdfvwZ9RXUo8xZFCMp2peEj5";
+const ANDREW = "7tuQQ9jaCeWmgGXa6y4BsBLbjF6gz6bruJ2paUNGowAC";
+const SAM = "DJMWKyzVNTqMuLh58NhETVEyp7sg41X142LnsuQwhwmu";
+const EDWARD = "E8xKtQZoHVjdtsm5mxvmufMt7SyptuiJiHKf26g8zTHw";
+const CARLOS = "D65bHwRCPjaKsS8ZakNLXkzDwkGcUpHADFqo8hXxuLPc";
+const OSCAR = "DnuuuuF6e6pFfkZkdECjY2gFUALuqWkzxGmXyFnd1aRt";
+const JALDHAR = "6ng1sB54ZLTi1SHmxUkdvKL2DHGr26LpAk6CA8iE9adb";
