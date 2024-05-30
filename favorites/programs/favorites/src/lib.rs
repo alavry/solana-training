34cb4a65206a1a25673ff5c1a3e602ae973012d2
Andrew Lavryshyn <alavry@users.noreply.github.com>
2024-05-30 12:48:13 -0400

    add favorites program

---
+++ favorites/programs/favorites/src/lib.rs
@@ -0,0 +1,59 @@
+use anchor_lang::prelude::*;
+
+// Don't need to manually change this
+declare_id!("NSijMqQBfiHVSseiyyyUGzr2pzuouiQKfrLC5GRM2ur");
+
+// Anchor accounts use 8 bytes to determine the account type
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String, hobbies: Vec<String>) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!("User {}'s favorite number is {}, favorite color is {}", user_public_key, number, color);
+
+        msg!("User's hobbies are: {:?}", hobbies);
+
+        context.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies,
+        });
+
+        Ok(())
+    }
+    
+    // We can also add a get_favorites instruction handler to get favorites
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>
+}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds=[b"favorites".as_ref(), user.key().as_ref()],
+    bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
---
+++ favorites/tests/favorites.ts
@@ -0,0 +1,80 @@
+
+import { Program, Wallet, AnchorProvider, setProvider, workspace, web3, getProvider, BN } from "@coral-xyz/anchor";
+import { airdropIfRequired } from "@solana-developers/helpers";
+import { Favorites } from "../target/types/favorites";
+import { assert } from "chai";
+
+describe("favorites", () => {
+  const provider = AnchorProvider.env();
+  setProvider(provider);
+  const user = (provider.wallet as Wallet).payer
+  const someRandomGuy = web3.Keypair.generate();
+  const program = workspace.Favorites as Program<Favorites>;
+  const favoriteNumber = new BN(23);
+  const favoriteColor = "purple";
+  const favoriteHobbies = ["skiing", "skydiving", "biking"];
+
+  before(async () => {
+    await airdropIfRequired(
+      getProvider().connection,
+      user.publicKey,
+      0.5 * web3.LAMPORTS_PER_SOL,
+      1 * web3.LAMPORTS_PER_SOL
+    );
+  })
+
+  it("Writes our favorites to the blockchain", async () => {
+    await program.methods
+      .setFavorites(favoriteNumber, favoriteColor, favoriteHobbies)
+      .signers([user])
+      .rpc();
+
+    const favoritesPdaAndBump = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId
+    );
+    
+    const favoritesPda = favoritesPdaAndBump[0];
+    
+    const dataFromPda = await program.account.favorites.fetch(favoritesPda);
+    
+    assert.equal(dataFromPda.color, favoriteColor);
+    assert.equal(dataFromPda.number.toString(), favoriteNumber.toString());
+    assert.deepEqual(dataFromPda.hobbies, favoriteHobbies);
+
+    try {
+      await program.methods.setFavorites(favoriteNumber, favoriteColor, favoriteHobbies)
+      .signers([someRandomGuy])
+      .rpc();
+    }
+    catch (error) {
+      const errorMessage = (error as Error).message;
+      assert.isTrue(errorMessage.includes("unknown signer"))
+    }
+  });
+
+  it("Checks that unknown signer are unable to write to the blockchain", async () => {
+    const favoritesPdaAndBump = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId
+    );
+    
+    const favoritesPda = favoritesPdaAndBump[0];
+    
+    const dataFromPda = await program.account.favorites.fetch(favoritesPda);
+    
+    assert.equal(dataFromPda.color, favoriteColor);
+    assert.equal(dataFromPda.number.toString(), favoriteNumber.toString());
+    assert.deepEqual(dataFromPda.hobbies, favoriteHobbies);
+
+    try {
+      await program.methods.setFavorites(favoriteNumber, favoriteColor, favoriteHobbies)
+      .signers([someRandomGuy])
+      .rpc();
+    }
+    catch (error) {
+      const errorMessage = (error as Error).message;
+      assert.isTrue(errorMessage.includes("unknown signer"))
+    }
+  });
+});
