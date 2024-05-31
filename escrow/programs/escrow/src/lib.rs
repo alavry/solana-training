623b5bc0b5b9dd709c9f0b2d446bdd29f7c24276
Andrew Lavryshyn <alavry@users.noreply.github.com>
2024-05-31 12:05:13 -0400

    dump it

---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,28 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("BVDNzU4SZWqW9qTsVWsAjY3vSb2TTicQNWvHsViN651j");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        offered_amount: u64,
+        wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context,
+            offered_amount)?;
+        instructions::make_offer::save_offer(context, id, wanted_amount)
+    }
+}
